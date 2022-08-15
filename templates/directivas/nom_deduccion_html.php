7e1f9ac0973a236f2e1f27f47b033ca8183e2bd7
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-15 11:15:00 -0500

    v.0.6.2 Se genero CRUD para la entidad nom_deduccion

---
+++ templates/directivas/nom_deduccion_html.php
@@ -0,0 +1,152 @@
+<?php
+namespace html;
+
+use gamboamartin\comercial\controllers\controlador_emempleado;
+use gamboamartin\empleado\controllers\controlador_em_empleado;
+use gamboamartin\errores\errores;
+use gamboamartin\nomina\controllers\controlador_nom_deduccion;
+use gamboamartin\nomina\controllers\controlador_nom_percepcion;
+use gamboamartin\system\html_controler;
+use models\cat_sat_tipo_percepcion_nom;
+use models\com_sucursal;
+use models\em_empleado;
+use models\nom_percepcion;
+use PDO;
+use stdClass;
+
+class nom_deduccion_html extends html_controler {
+
+    private function asigna_inputs(controlador_nom_deduccion $controler, stdClass $inputs): array|stdClass
+    {
+        $controler->inputs->select = new stdClass();
+        $controler->inputs->select->cat_sat_tipo_deduccion_nom_id = $inputs->selects->cat_sat_tipo_deduccion_nom_id;
+
+        return $controler->inputs;
+    }
+
+    public function genera_inputs_alta(controlador_nom_deduccion $controler, PDO $link): array|stdClass
+    {
+        $inputs = $this->init_alta(link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+
+        }
+        $inputs_asignados = $this->asigna_inputs(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
+
+    private function genera_inputs_modifica(controlador_nom_deduccion $controler,PDO $link,
+                                            stdClass $params = new stdClass()): array|stdClass
+    {
+        $inputs = $this->init_modifica(link: $link, row_upd: $controler->row_upd, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+
+        }
+        $inputs_asignados = $this->asigna_inputs(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
+
+    private function init_alta(PDO $link): array|stdClass
+    {
+        $selects = $this->selects_alta(link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar selects',data:  $selects);
+        }
+
+        $texts = $this->texts_alta(row_upd: new stdClass(), value_vacio: true);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar texts',data:  $texts);
+        }
+
+        $alta_inputs = new stdClass();
+        $alta_inputs->selects = $selects;
+        $alta_inputs->texts = $texts;
+
+        return $alta_inputs;
+    }
+
+    private function init_modifica(PDO $link, stdClass $row_upd, stdClass $params = new stdClass()): array|stdClass
+    {
+        $selects = $this->selects_modifica(link: $link, row_upd: $row_upd);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar selects',data:  $selects);
+        }
+
+        $texts = $this->texts_alta(row_upd: $row_upd, value_vacio: false, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar texts',data:  $texts);
+        }
+
+        $alta_inputs = new stdClass();
+        $alta_inputs->texts = $texts;
+        $alta_inputs->selects = $selects;
+        return $alta_inputs;
+    }
+
+    public function inputs_nom_deduccion(controlador_nom_deduccion $controlador,
+                                       stdClass $params = new stdClass()): array|stdClass
+    {
+        $inputs = $this->genera_inputs_modifica(controler: $controlador,
+            link: $controlador->link, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+        }
+        return $inputs;
+    }
+
+    private function selects_alta(PDO $link): array|stdClass
+    {
+        $selects = new stdClass();
+
+        $select = (new cat_sat_tipo_deduccion_nom_html(html:$this->html_base))->select_cat_sat_tipo_deduccion_nom_id(
+            cols: 12, con_registros:true, id_selected:-1,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+        }
+        $selects->cat_sat_tipo_deduccion_nom_id = $select;
+
+        return $selects;
+    }
+
+    private function selects_modifica(PDO $link, stdClass $row_upd): array|stdClass
+    {
+        $selects = new stdClass();
+
+        $select = (new cat_sat_tipo_deduccion_nom_html(html:$this->html_base))->select_cat_sat_tipo_deduccion_nom_id(
+            cols: 12, con_registros:true, id_selected:$row_upd->cat_sat_tipo_deduccion_nom_id,link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+        }
+        $selects->cat_sat_tipo_deduccion_nom_id = $select;
+
+        return $selects;
+    }
+
+    public function select_nom_deduccion_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    {
+        $modelo = new nom_percepcion(link: $link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo,label: 'Percepcion',required: true);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
+
+    private function texts_alta(stdClass $row_upd, bool $value_vacio, stdClass $params = new stdClass()): array|stdClass
+    {
+        $texts = new stdClass();
+        return $texts;
+    }
+
+}
---
+++ templates/inputs/nom_deduccion/alta.php
@@ -0,0 +1,8 @@
+<?php /** @var  \gamboamartin\nomina\controllers\controlador_nom_deduccion $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+<?php echo $controlador->inputs->codigo; ?>
+<?php echo $controlador->inputs->codigo_bis; ?>
+<?php echo $controlador->inputs->descripcion; ?>
+<?php echo $controlador->inputs->select->cat_sat_tipo_deduccion_nom_id; ?>
+
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
