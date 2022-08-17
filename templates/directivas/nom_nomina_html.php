dd8551a35bfc93b8499029447074d00bcbc55149
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-17 15:06:35 -0500

    v.0.20.4 Se integran valores por defecto en campos fecha

--- templates/directivas/nom_nomina_html.php
+++ templates/directivas/nom_nomina_html.php
@@ -491,19 +491,25 @@ class nom_nomina_html extends html_controler
         }
         $texts->fecha = $in_fecha;
 
-        $in_fecha_inicial_pago = $this->input_fecha_inicial_pago(cols: 4, row_upd: $row_upd, value_vacio: $value_vacio);
+        $row_upd->fecha_inicial_pago = date('Y-m-d');
+
+        $in_fecha_inicial_pago = $this->input_fecha_inicial_pago(cols: 4, row_upd: $row_upd, value_vacio: false);
         if (errores::$error) {
             return $this->error->error(mensaje: 'Error al generar input', data: $in_fecha_inicial_pago);
         }
         $texts->fecha_inicial_pago = $in_fecha_inicial_pago;
 
-        $in_fecha_final_pago = $this->input_fecha_final_pago(cols: 4, row_upd: $row_upd, value_vacio: $value_vacio);
+        $row_upd->fecha_final_pago = date('Y-m-d');
+
+        $in_fecha_final_pago = $this->input_fecha_final_pago(cols: 4, row_upd: $row_upd, value_vacio: false);
         if (errores::$error) {
             return $this->error->error(mensaje: 'Error al generar input', data: $in_fecha_final_pago);
         }
         $texts->fecha_final_pago = $in_fecha_final_pago;
 
-        $in_fecha_pago = $this->input_fecha_pago(cols: 4, row_upd: $row_upd, value_vacio: $value_vacio);
+        $row_upd->fecha_pago = date('Y-m-d');
+
+        $in_fecha_pago = $this->input_fecha_pago(cols: 4, row_upd: $row_upd, value_vacio: false);
         if (errores::$error) {
             return $this->error->error(mensaje: 'Error al generar input', data: $in_fecha_pago);
         }
