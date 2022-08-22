f808d634fb0ce7ceb942ead44695e00f783b7467
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-22 13:05:12 -0500

    v.0.32.1 FIX integracion get cuentas bancarias

---
+++ controllers/controlador_em_cuenta_bancaria.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\nomina\controllers;
+
+
+
+class controlador_em_cuenta_bancaria extends \gamboamartin\empleado\controllers\controlador_em_cuenta_bancaria {
+
+
+
+
+}
