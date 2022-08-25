0962e5707d7637d94666d28e3cde9a75138e67c6
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-25 14:28:41 -0500

    v.0.58.1 integra fecha get isr

--- orm/nom_nomina.php
+++ orm/nom_nomina.php
@@ -301,14 +301,19 @@ class nom_nomina extends modelo
         return $this->registro;
     }
 
-    private function get_isr(int $cat_sat_periodicidad_pago_nom_id, float|int $monto){
+    private function get_isr(int $cat_sat_periodicidad_pago_nom_id, float|int $monto, string $fecha = ''){
         $filtro['cat_sat_periodicidad_pago_nom.id'] = $cat_sat_periodicidad_pago_nom_id;
 
+        if($fecha === ''){
+            $fecha = date('Y-m-d');
+        }
 
-        $filtro_especial = $this->filtro_especial_isr(monto: $monto);
+        $filtro_especial = $this->filtro_especial_isr(monto: $monto, fecha : $fecha);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al obtener filtro', data: $filtro_especial);
         }
+
+
 
         $r_isr = (new cat_sat_isr($this->link))->filtro_and(filtro: $filtro, filtro_especial: $filtro_especial);
         if(errores::$error){
@@ -346,14 +351,19 @@ class nom_nomina extends modelo
         return $r_alta_factura;
     }
 
-    public function isr(int $cat_sat_periodicidad_pago_nom_id, float|int $monto): float|array
+    public function isr(int $cat_sat_periodicidad_pago_nom_id, float|int $monto, string $fecha = ''): float|array
     {
         if($cat_sat_periodicidad_pago_nom_id<=0){
             return $this->error->error(mensaje: 'Error $cat_sat_periodicidad_pago_nom_id debe ser mayor a 0',
                 data: $cat_sat_periodicidad_pago_nom_id);
         }
 
-        $row_isr = $this->get_isr(cat_sat_periodicidad_pago_nom_id: $cat_sat_periodicidad_pago_nom_id, monto:$monto);
+        if($fecha === ''){
+            $fecha = date('Y-m-d');
+        }
+
+        $row_isr = $this->get_isr(cat_sat_periodicidad_pago_nom_id: $cat_sat_periodicidad_pago_nom_id, monto:$monto,
+            fecha: $fecha);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al obtener isr', data: $row_isr);
         }
@@ -362,7 +372,6 @@ class nom_nomina extends modelo
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al calcular isr', data: $isr);
         }
-
 
         return $isr;
 
@@ -387,17 +396,31 @@ class nom_nomina extends modelo
         return $codigo;
     }
 
-    private function filtro_especial_isr(float $monto): array
+    private function filtro_especial_isr(float $monto, string $fecha = ''): array
     {
-        $filtro_especial[0][(string)$monto]['operador'] = '>=';
-        $filtro_especial[0][(string)$monto]['valor'] = 'cat_sat_isr.limite_inferior';
-        $filtro_especial[0][(string)$monto]['comparacion'] = 'AND';
-        $filtro_especial[0][(string)$monto]['valor_es_campo'] = true;
+        if($fecha === ''){
+            $fecha = date('Y-m-d');
+        }
 
-        $filtro_especial[1][(string)$monto]['operador'] = '<=';
-        $filtro_especial[1][(string)$monto]['valor'] = 'cat_sat_isr.limite_superior';
-        $filtro_especial[1][(string)$monto]['comparacion'] = 'AND';
-        $filtro_especial[1][(string)$monto]['valor_es_campo'] = true;
+        $filtro_especial[0][$fecha]['operador'] = '>=';
+        $filtro_especial[0][$fecha]['valor'] = 'cat_sat_isr.fecha_inicio';
+        $filtro_especial[0][$fecha]['comparacion'] = 'AND';
+        $filtro_especial[0][$fecha]['valor_es_campo'] = true;
+
+        $filtro_especial[1][$fecha]['operador'] = '<=';
+        $filtro_especial[1][$fecha]['valor'] = 'cat_sat_isr.fecha_fin';
+        $filtro_especial[1][$fecha]['comparacion'] = 'AND';
+        $filtro_especial[1][$fecha]['valor_es_campo'] = true;
+
+        $filtro_especial[2][(string)$monto]['operador'] = '>=';
+        $filtro_especial[2][(string)$monto]['valor'] = 'cat_sat_isr.limite_inferior';
+        $filtro_especial[2][(string)$monto]['comparacion'] = 'AND';
+        $filtro_especial[2][(string)$monto]['valor_es_campo'] = true;
+
+        $filtro_especial[3][(string)$monto]['operador'] = '<=';
+        $filtro_especial[3][(string)$monto]['valor'] = 'cat_sat_isr.limite_superior';
+        $filtro_especial[3][(string)$monto]['comparacion'] = 'AND';
+        $filtro_especial[3][(string)$monto]['valor_es_campo'] = true;
 
         return $filtro_especial;
     }
