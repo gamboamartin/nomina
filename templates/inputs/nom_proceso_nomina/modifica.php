d679fd267b45b8206f4a4565d258e747006cd5de
Kevin <kevin.acuna@ivitec.com.mx>
2022-09-06 14:54:29 -0500

    v.0.268.7 Se genera CRUD para nom_proceso_nomina

---
+++ templates/inputs/nom_proceso_nomina/modifica.php
@@ -0,0 +1,11 @@
+<?php /** @var  \gamboamartin\nomina\controllers\controlador_nom_proceso_nomina $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+<?php echo $controlador->inputs->id; ?>
+<?php echo $controlador->inputs->codigo; ?>
+<?php echo $controlador->inputs->codigo_bis; ?>
+<?php echo $controlador->inputs->descripcion; ?>
+<?php echo $controlador->inputs->descripcion_select; ?>
+<?php echo $controlador->inputs->alias; ?>
+<?php echo $controlador->inputs->select->adm_seccion_id; ?>
+<?php echo $controlador->inputs->select->pr_tipo_proceso_id; ?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
