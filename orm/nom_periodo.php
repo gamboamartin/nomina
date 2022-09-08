0ac31579fbc3071627b45774f19bf6b265338555
Kevin <kevin.acuna@ivitec.com.mx>
2022-09-08 09:59:44 -0500

    v.0.286.8 Se genera accion de eliminacion de nominas por periodo

--- orm/nom_periodo.php
+++ orm/nom_periodo.php
@@ -2,6 +2,7 @@
 namespace models;
 use base\orm\modelo;
 use gamboamartin\errores\errores;
+use html\nom_nomina_html;
 use PDO;
 use stdClass;
 
@@ -60,6 +61,33 @@ class nom_periodo extends nominas_confs {
         return $r_alta_bd;
     }
 
+    public function elimina_bd(int $id): array
+    {
+        $filtro['nom_nomina.nom_periodo_id'] = $id;
+        $registros_nomina_periodo = (new nom_nomina($this->link))->filtro_and( filtro: $filtro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al obtener las nominas ligadas al periodo',
+                data: $registros_nomina_periodo);
+        }
+
+        foreach ($registros_nomina_periodo->registros as $nomina) {
+            $elimina = $this->elimina_nomina_periodo(nomina_id: $nomina['nom_nomina_id']);
+            if (errores::$error) {
+                return $this->error->error(mensaje: 'Error al eliminar la nomina ligada al periodo', data: $elimina);
+            }
+        }
+
+        return parent::elimina_bd($id);
+    }
+
+    private function elimina_nomina_periodo(int $nomina_id) : array|stdClass{
+        $elimina_bd = (new nom_nomina(link: $this->link))->elimina_bd($nomina_id);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al eliminar nomina', data: $elimina_bd);
+        }
+        return $elimina_bd;
+    }
+
     /**
      * Obtiene un conjunto de empleados de un registro patronal
      * @param int $im_registro_patronal_id Registro patronal integrado
@@ -116,20 +144,18 @@ class nom_periodo extends nominas_confs {
 
     private function genera_registro_nomina_empleado(mixed $em_empleado, mixed $nom_periodo) : array{
 
-
-
         $registros['im_registro_patronal_id'] = $em_empleado['im_registro_patronal_id'];
         $registros['em_empleado_id'] = $em_empleado['em_empleado_id'];
         $registros['nom_conf_empleado_id'] = 1;
         $registros['em_cuenta_bancaria_id'] = 1;
         $registros['folio'] = rand();
         $registros['fecha'] = $nom_periodo['nom_periodo_fecha_pago'];
-        $registros['cat_sat_tipo_nomina_id'] = $nom_periodo['nom_periodo_cat_sat_tipo_nomina_id'];
+        $registros['cat_sat_tipo_nomina_id'] = 1;
         $registros['cat_sat_periodicidad_pago_nom_id'] = $nom_periodo['nom_periodo_cat_sat_periodicidad_pago_nom_id'];
         $registros['fecha_pago'] =$nom_periodo['nom_periodo_fecha_pago'];
         $registros['fecha_inicial_pago'] = $nom_periodo['nom_periodo_fecha_inicial_pago'];
         $registros['fecha_final_pago'] = $nom_periodo['nom_periodo_fecha_final_pago'];
-        $registros['num_dias_pagados'] = $nom_periodo['cat_sat_periodicidad_pago_nom_n_dias'];
+        $registros['num_dias_pagados'] = 15;
         $registros['nom_periodo_id'] = $nom_periodo['nom_periodo_id'];
         $registros['descuento'] = 0;
 
