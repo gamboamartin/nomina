5e7e737ff0ccd10f6dec7e04b681d9e86e233184
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-26 15:49:07 -0500

    v0.379.20 Se integra calculo invalidez y vida

--- controllers/controlador_nom_nomina.php
+++ controllers/controlador_nom_nomina.php
@@ -693,6 +693,24 @@ class controlador_nom_nomina extends base_nom
 
         $this->cuotas_obrero_patronales->registros[] = $cuota;
 
+        $factor_invalidez_vida = 1.75;
+        $n_dias_trabajados = $this->registro['nom_nomina_num_dias_pagados'];
+        $salario_base_cotizacion = $this->registro['em_empleado_salario_diario_integrado'];
+
+        $cuota_riesgo_trabajo = (new im_movimiento($this->link))->calcula_invalidez_vida(
+            factor_invalidez_vida: $factor_invalidez_vida, n_dias_trabajados: $n_dias_trabajados,
+            salario_base_cotizacion: $salario_base_cotizacion);
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al obtener partidas', data: $partidas, header: $header, ws: $ws);
+        }
+
+        $cuota = array();
+        $cuota['concepto'] = 'Invalidez y Vida';
+        $cuota['prestaciones'] = 'Invalidez y Vida';
+        $cuota['monto'] = $cuota_riesgo_trabajo;
+
+        $this->cuotas_obrero_patronales->registros[] = $cuota;
+
         return $base->template;
     }
 
