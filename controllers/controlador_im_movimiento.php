a0580fd50cf8a64672dbef5f0d04341d14363ebe
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-26 13:20:16 -0500

    v0.375.20 Se integra controler movimiento

---
+++ controllers/controlador_im_movimiento.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\nomina\controllers;
+
+
+
+class controlador_im_movimiento extends \gamboamartin\im_registro_patronal\controllers\controlador_im_movimiento {
+
+}
