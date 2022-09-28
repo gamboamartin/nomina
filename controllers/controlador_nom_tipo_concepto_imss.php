06cb845898e64e4287f92e0f9c36bf65a8ed1208
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-28 10:52:13 -0500

    v0.387.21 Se integra crud de concepto_imss

---
+++ controllers/controlador_nom_tipo_concepto_imss.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\nomina\controllers;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use gamboamartin\template\html;
+use html\nom_tipo_concepto_imss_html;
+use models\nom_tipo_concepto_imss;
+use PDO;
+use stdClass;
+
+class controlador_nom_tipo_concepto_imss extends system {
+
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
+        $modelo = new nom_tipo_concepto_imss(link: $link);
+        $html_ = new nom_tipo_concepto_imss_html(html: $html);
+        $obj_link = new links_menu($this->registro_id);
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'Tipo Concepto';
+    }
+
+}
---
+++ templates/directivas/nom_tipo_concepto_imss_html.php
@@ -0,0 +1,24 @@
+<?php
+namespace html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\html_controler;
+use models\nom_tipo_concepto_imss;
+use PDO;
+use stdClass;
+
+
+class nom_tipo_concepto_imss_html extends html_controler {
+
+    public function select_nom_tipo_concepto_imss_id(int $cols,bool $con_registros,int $id_selected, PDO $link): array|string
+    {
+        $modelo = new nom_tipo_concepto_imss($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo,label: 'Tipo Concepto',required: true);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
+}
