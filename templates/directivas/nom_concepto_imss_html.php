4e65d494de1990450a7bad07f61b1e117e3a6dd5
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-28 11:35:20 -0500

    v0.389.21 Se integra view modifica nom_concepto_imss

--- templates/directivas/nom_concepto_imss_html.php
+++ templates/directivas/nom_concepto_imss_html.php
@@ -48,6 +48,21 @@ class nom_concepto_imss_html extends html_controler {
         return $inputs_asignados;
     }
 
+    private function genera_inputs_modifica(controlador_nom_concepto_imss $controler,PDO $link,
+                                            stdClass $params = new stdClass()): array|stdClass
+    {
+        $inputs = $this->init_modifica(link: $link, row_upd: $controler->row_upd, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+
+        }
+        $inputs_asignados = $this->asigna_inputs(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
 
     protected function init_alta(array $keys_selects, PDO $link): array|stdClass
     {
@@ -66,6 +81,68 @@ class nom_concepto_imss_html extends html_controler {
         return $init;
     }
 
+    private function init_modifica(PDO $link, stdClass $row_upd, stdClass $params = new stdClass()): array|stdClass
+    {
+
+        $selects = $this->selects_modifica(link: $link, row_upd: $row_upd,params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar selects',data:  $selects);
+        }
+
+
+        $texts = $this->texts_modifica(row_upd: $row_upd, value_vacio: false, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar texts',data:  $texts);
+        }
+
+        $alta_inputs = new stdClass();
+        $alta_inputs->texts = $texts;
+        $alta_inputs->selects = $selects;
+        return $alta_inputs;
+    }
+
+    private function selects_modifica(PDO $link, stdClass $row_upd, stdClass $params = new stdClass()): array|stdClass
+    {
+
+        $cols_nom_nomina_id = $params->nom_nomina_id->cols ?? 4;
+        $disabled_nom_nomina_id = $params->nom_nomina_id->disabled ?? false;
+        $filtro_nom_nomina_id = $params->nom_nomina_id->filtro ?? array();
+
+        $selects = new stdClass();
+
+        $select = (new nom_nomina_html(html:$this->html_base))->select_nom_nomina_id(
+            cols: $cols_nom_nomina_id, con_registros:true, id_selected: $row_upd->nom_nomina_id,link: $link,
+            disabled: $disabled_nom_nomina_id, filtro: $filtro_nom_nomina_id);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+        }
+        $selects->nom_nomina_id = $select;
+
+        $cols_nom_tipo_concepto_imss_id = $params->nom_tipo_concepto_imss_id->cols ?? 4;
+        $select = (new nom_tipo_concepto_imss_html(html:$this->html_base))->select_nom_tipo_concepto_imss_id(
+            cols: $cols_nom_tipo_concepto_imss_id, con_registros:true, id_selected:$row_upd->nom_tipo_concepto_imss_id,
+            link: $link);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select',data:  $select);
+        }
+
+        $selects->nom_tipo_concepto_imss_id = $select;
+
+        return $selects;
+    }
+
+
+    public function inputs_nom_concepto_imss(controlador_nom_concepto_imss $controlador,
+                                              stdClass $params = new stdClass()): array|stdClass
+    {
+        $inputs = $this->genera_inputs_modifica(controler: $controlador,
+            link: $controlador->link, params: $params);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+        }
+        return $inputs;
+    }
+
     protected function texts_alta(stdClass $row_upd, bool $value_vacio, stdClass $params = new stdClass()): array|stdClass
     {
         $texts = new stdClass();
@@ -78,6 +155,19 @@ class nom_concepto_imss_html extends html_controler {
 
         $texts->monto = $in_monto;
 
+        return $texts;
+    }
+
+    private function texts_modifica(stdClass $row_upd, bool $value_vacio, stdClass $params = new stdClass()): array|stdClass
+    {
+        $texts = new stdClass();
+
+        $in_monto = $this->input_monto(cols: 4, row_upd:  $row_upd,value_vacio:  false);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar input',data:  $in_monto);
+        }
+
+        $texts->monto = $in_monto;
         return $texts;
     }
 
--- templates/inputs/nom_concepto_imss/modifica.php
+++ templates/inputs/nom_concepto_imss/modifica.php
@@ -1,10 +1,10 @@
 <?php /** @var  \gamboamartin\nomina\controllers\controlador_nom_par_percepcion $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
+<?php echo $controlador->inputs->id; ?>
 <?php echo $controlador->inputs->codigo; ?>
 <?php echo $controlador->inputs->codigo_bis; ?>
 <?php echo $controlador->inputs->descripcion; ?>
 <?php echo $controlador->inputs->select->nom_nomina_id; ?>
-<?php echo $controlador->inputs->select->nom_percepcion_id; ?>
-<?php echo $controlador->inputs->importe_gravado; ?>
-<?php echo $controlador->inputs->importe_exento; ?>
+<?php echo $controlador->inputs->select->nom_tipo_concepto_imss_id; ?>
+<?php echo $controlador->inputs->monto; ?>
 <?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
