2cc191fcb0e1dc528feb524b47a08e5da528c8f2
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-29 11:08:59 -0500

    v0.399.21 Se integra view sube_archivo

---
+++ views/nom_periodo/sube_archivo.php
@@ -0,0 +1,36 @@
+<?php /** @var \gamboamartin\nomina\controllers\controlador_nom_periodo $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+
+<main class="main section-color-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+
+                <?php include (new views())->ruta_templates."head/title.php"; ?>
+                <?php include (new views())->ruta_templates."mensajes.php"; ?>
+            </div>
+            <div class="col-lg-12">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+
+                          
+                        </div><!-- /.center-content -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+    <br>
+
+
+
+</main>
+
+
+
+
+
