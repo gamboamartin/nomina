0aeb4e6d1932c100dd38023cc33ed4b4d7f3171a
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-03 13:13:12 -0500

    v0.423.21 Se genera CRUD tipo_incidencia

---
+++ templates/inputs/nom_tipo_incidencia/alta.php
@@ -0,0 +1,8 @@
+<?php /** @var  \gamboamartin\nomina\controllers\controlador_nom_tipo_incidencia $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+<?php echo $controlador->inputs->codigo; ?>
+<?php echo $controlador->inputs->codigo_bis; ?>
+<?php echo $controlador->inputs->descripcion; ?>
+<?php echo $controlador->inputs->descripcion_select; ?>
+<?php echo $controlador->inputs->alias; ?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
