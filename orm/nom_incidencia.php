5c0401c7b22f864f4ae9744e57412e1cc4035906
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-10-05 09:19:55 -0500

    v0.435.21 Se sobreescribe metodo de alta en incidencia

--- orm/nom_incidencia.php
+++ orm/nom_incidencia.php
@@ -1,7 +1,9 @@
 <?php
 namespace models;
 use base\orm\modelo;
+use gamboamartin\errores\errores;
 use PDO;
+use stdClass;
 
 class nom_incidencia extends modelo{
 
@@ -14,5 +16,31 @@ class nom_incidencia extends modelo{
             columnas: $columnas);
 
         $this->NAMESPACE = __NAMESPACE__;
+    }
+
+    public function alta_bd(): array|stdClass
+    {
+        if (!isset($this->registro['codigo'])) {
+            $this->registro['codigo'] = $this->registro['nom_tipo_incidencia_id'] . ' - ' .
+                $this->registro['em_empleado_id'] . ' - ' . rand();
+        }
+        if (!isset($this->registro['codigo_bis'])) {
+            $this->registro['codigo_bis'] = $this->registro['codigo'];
+        }
+
+        if (!isset($this->registro['descripcion'])) {
+            $this->registro['descripcion'] = $this->registro['n_dias'].' - '.
+                $this->registro['nom_tipo_incidencia_id'] . ' - ' . $this->registro['em_empleado_id'] . ' - ' . rand();
+        }
+        if (!isset($this->registro['descripcion_select'])) {
+            $this->registro['descripcion_select'] = $this->registro['descripcion'];
+        }
+
+        $r_alta = parent::alta_bd();
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al dar de alta incidencias', data: $r_alta);
+        }
+
+        return $r_alta;
     }
 }
