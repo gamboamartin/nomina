d5ac8192fcce9b6a38c8d56a321b2c5cb72e1110
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-11 12:06:44 -0500

    v0.469.26 Se ajustan campos en alta layout

--- orm/nom_layout.php
+++ orm/nom_layout.php
@@ -10,8 +10,17 @@ class nom_layout extends modelo{
         $columnas = array($tabla=>false);
         $campos_obligatorios = array();
 
+        $campos_view = array();
+        $campos_view['bn_sucursal_id']['type'] = 'selects';
+        $campos_view['bn_sucursal_id']['model'] = (new bn_sucursal($link));
+
+        $campos_view['nom_periodo_id']['type'] = 'selects';
+        $campos_view['nom_periodo_id']['model'] = (new nom_periodo($link));
+
+        $campos_view['fecha_pago']['type'] = 'inputs';
+
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, campos_view: $campos_view);
 
         $this->NAMESPACE = __NAMESPACE__;
     }
--- templates/directivas/nom_layout_html.php
+++ templates/directivas/nom_layout_html.php
@@ -1,13 +1,41 @@
 <?php
 namespace html;
 
+use base\orm\modelo;
 use gamboamartin\errores\errores;
+use gamboamartin\nomina\controllers\controlador_nom_layout;
 use gamboamartin\system\html_controler;
 use models\nom_layout;
 use PDO;
 use stdClass;
 
 class nom_layout_html extends html_controler {
+
+    private function asigna_inputs_alta(controlador_nom_layout $controler, array|stdClass $inputs): array|stdClass
+    {
+        $controler->inputs->select = new stdClass();
+        $controler->inputs->select->bn_sucursal_id = $inputs['selects']->bn_sucursal_id;
+        $controler->inputs->select->nom_periodo_id = $inputs['selects']->nom_periodo_id;
+
+        $controler->inputs->fecha_pago = $inputs['inputs']->fecha_pago;
+
+        return $controler->inputs;
+    }
+
+    public function genera_inputs_alta(controlador_nom_layout $controler, modelo $modelo, PDO $link, array $keys_selects = array()): array|stdClass
+    {
+        $inputs = $this->init_alta2(row_upd: $controler->row_upd,modelo: $controler->modelo,link: $link,keys_selects:  $keys_selects);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar inputs',data:  $inputs);
+
+        }
+        $inputs_asignados = $this->asigna_inputs_alta(controler:$controler, inputs: $inputs);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar inputs',data:  $inputs_asignados);
+        }
+
+        return $inputs_asignados;
+    }
 
     public function select_nom_layout_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
                                          bool $disabled = false, array $filtro = array()): array|string
