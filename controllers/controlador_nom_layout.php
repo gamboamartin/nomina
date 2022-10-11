0530a538b5946882848b6f9c60183d98a070b4c3
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-11 11:55:43 -0500

    v0.468.26 Se genera CRUD Layout

---
+++ controllers/controlador_nom_layout.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace gamboamartin\nomina\controllers;
+
+use gamboamartin\errores\errores;
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use gamboamartin\template\html;
+use html\nom_layout_html;
+use models\nom_layout;
+use PDO;
+use stdClass;
+
+class controlador_nom_layout extends system {
+
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass()){
+        $modelo = new nom_layout(link: $link);
+        $html_ = new nom_layout_html(html: $html);
+        $obj_link = new links_menu($this->registro_id);
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'Layout';
+    }
+
+}
