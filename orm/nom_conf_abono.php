4752269dcbfc6ca8ccbf62493136c49bddcd6c9a
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-02 17:14:44 -0500

    v0.532.38 Se ajusta nom_conf_abono

--- orm/nom_conf_abono.php
+++ orm/nom_conf_abono.php
@@ -3,6 +3,7 @@ namespace models;
 use base\orm\modelo;
 
 use gamboamartin\empleado\models\em_tipo_abono_anticipo;
+use gamboamartin\empleado\models\em_tipo_anticipo;
 use gamboamartin\errores\errores;
 
 use PDO;
@@ -12,24 +13,27 @@ class nom_conf_abono extends modelo{
 
     public function __construct(PDO $link){
         $tabla = 'nom_conf_abono';
-        $columnas = array($tabla=>false, 'em_tipo_abono_anticipo'=>$tabla, 'nom_deduccion'=>$tabla, 'adm_campo'=>$tabla);
+
+        $columnas = array($tabla=>false, 'em_tipo_abono_anticipo'=>$tabla, 'em_tipo_anticipo'=>$tabla,
+            'nom_deduccion'=>$tabla, 'adm_campo'=>$tabla);
+
         $campos_obligatorios = array('descripcion','codigo','descripcion_select','alias','codigo_bis',
             'em_tipo_abono_anticipo_id','nom_deduccion_id');
-        $campos_view = array(
-            'em_tipo_abono_anticipo_id' => array('type' => 'selects', 'model' => new em_tipo_abono_anticipo($link)),
-            'nom_deduccion_id' => array('type' => 'selects', 'model' => new nom_deduccion($link)),
-            'id' => array('type' => 'inputs'),'codigo' => array('type' => 'inputs'));
+
+        $campos_view['em_tipo_abono_anticipo_id'] = array('type' => 'selects', 'model' => new em_tipo_abono_anticipo($link));
+        $campos_view['nom_deduccion_id'] = array('type' => 'selects', 'model' => new nom_deduccion($link));
+        $campos_view['em_tipo_anticipo_id'] = array('type' => 'selects', 'model' => new em_tipo_anticipo($link));
+        $campos_view['adm_campo_id'] = array('type' => 'selects', 'model' => new adm_campo($link));
+        $campos_view['codigo'] = array('type' => 'inputs');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas,campos_view: $campos_view);
 
         $this->NAMESPACE = __NAMESPACE__;
-
     }
 
     public function alta_bd(): array|stdClass
     {
-
         if (!isset($this->registro['descripcion_select'])) {
             $this->registro['descripcion_select'] = $this->registro['descripcion'];
         }
--- templates/inputs/nom_conf_abono/alta.php
+++ templates/inputs/nom_conf_abono/alta.php
@@ -1,7 +1,9 @@
 <?php /** @var  \gamboamartin\nomina\controllers\controlador_nom_conf_abono $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
 <?php echo $controlador->inputs->codigo; ?>
-<?php echo $controlador->inputs->select->em_tipo_abono_anticipo_id; ?>
+<?php echo $controlador->inputs->adm_campo_id; ?>
+<?php echo $controlador->inputs->em_tipo_anticipo_id; ?>
+<?php echo $controlador->inputs->em_tipo_abono_anticipo_id; ?>
 <?php echo $controlador->inputs->descripcion; ?>
-<?php echo $controlador->inputs->select->nom_deduccion_id; ?>
+<?php echo $controlador->inputs->nom_deduccion_id; ?>
 <?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
