52a6a268e903e6d22d50529f9ff91f9aef234ae4
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-03 15:22:30 -0500

    v0.536.38 Se ajusta nom_conf_factura

--- orm/nom_conf_factura.php
+++ orm/nom_conf_factura.php
@@ -14,24 +14,21 @@ class nom_conf_factura extends modelo{
 
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla => false, "cat_sat_forma_pago" => $tabla, "cat_sat_metodo_pago" => $tabla,
+            "cat_sat_moneda" => $tabla, "com_tipo_cambio" => $tabla, "cat_sat_uso_cfdi" => $tabla,
+            "cat_sat_tipo_de_comprobante" => $tabla, "com_producto" => $tabla);
         $campos_obligatorios = array('cat_sat_forma_pago_id','cat_sat_metodo_pago_id','cat_sat_moneda_id',
             'com_tipo_cambio_id','cat_sat_uso_cfdi_id','cat_sat_tipo_de_comprobante_id','com_producto_id');
 
-        $campos_view['cat_sat_forma_pago_id']['type'] = "selects";
-        $campos_view['cat_sat_forma_pago_id']['model'] = (new cat_sat_forma_pago($link));
-        $campos_view['cat_sat_metodo_pago_id']['type'] = "selects";
-        $campos_view['cat_sat_metodo_pago_id']['model'] = (new cat_sat_metodo_pago($link));
-        $campos_view['cat_sat_moneda_id']['type'] = "selects";
-        $campos_view['cat_sat_moneda_id']['model'] = (new cat_sat_moneda($link));
-        $campos_view['com_tipo_cambio_id']['type'] = "selects";
-        $campos_view['com_tipo_cambio_id']['model'] = (new com_tipo_cambio($link));
-        $campos_view['cat_sat_uso_cfdi_id']['type'] = "selects";
-        $campos_view['cat_sat_uso_cfdi_id']['model'] = (new cat_sat_uso_cfdi($link));
-        $campos_view['cat_sat_tipo_de_comprobante_id']['type'] = "selects";
-        $campos_view['cat_sat_tipo_de_comprobante_id']['model'] = (new cat_sat_tipo_de_comprobante($link));
-        $campos_view['com_producto_id']['type'] = "selects";
-        $campos_view['com_producto_id']['model'] = (new com_producto($link));
+        $campos_view['cat_sat_forma_pago_id'] = array('type' => 'selects', 'model' => new cat_sat_forma_pago($link));
+        $campos_view['cat_sat_metodo_pago_id'] = array('type' => 'selects', 'model' => new cat_sat_metodo_pago($link));
+        $campos_view['cat_sat_moneda_id'] = array('type' => 'selects', 'model' => new cat_sat_moneda($link));
+        $campos_view['com_tipo_cambio_id'] = array('type' => 'selects', 'model' => new com_tipo_cambio($link));
+        $campos_view['cat_sat_uso_cfdi_id'] = array('type' => 'selects', 'model' => new cat_sat_uso_cfdi($link));
+        $campos_view['cat_sat_tipo_de_comprobante_id'] = array('type' => 'selects', 'model' => new cat_sat_tipo_de_comprobante($link));
+        $campos_view['com_producto_id'] = array('type' => 'selects', 'model' => new com_producto($link));
+        $campos_view['codigo'] = array('type' => 'inputs');
+        $campos_view['codigo_bis'] = array('type' => 'inputs');
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas,campos_view: $campos_view);
