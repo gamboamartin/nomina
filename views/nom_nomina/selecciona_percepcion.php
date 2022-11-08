e8e1d36fb8da65af6e379b2613a706958be20f35
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-11-08 12:48:03 -0600

    v0.530.38 Se ajusta vista de seleccion perp

---
+++ views/nom_nomina/selecciona_percepcion.php
@@ -0,0 +1,28 @@
+<?php /** @var \gamboamartin\nomina\controllers\controlador_nom_nomina $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+
+<main class="main section-color-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
+                    <?php include (new views())->ruta_templates."head/title.php"; ?>
+                    <?php include (new views())->ruta_templates."head/subtitulo.php"; ?>
+                    <?php include (new views())->ruta_templates."mensajes.php"; ?>
+                    <form method="post" action="<?php echo $controlador->link_percepcion_neto_alta; ?>" class="form-additional">
+                        <?php echo $controlador->inputs->select->nom_percepcion_id; ?>
+                        <?php echo $controlador->inputs->importe_excedente; ?>
+                        <div class="control-group btn-alta">
+                            <div class="controls">
+                                <button type="submit" class="btn btn-success" value="modifica" name="btn_action_next">Alta</button><br>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+</main>
