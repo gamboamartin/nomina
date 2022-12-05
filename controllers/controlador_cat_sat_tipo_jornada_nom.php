81ecedf7e4b891f9c60bd98e73b58e009779cb1b
mgamboa <gamboa.martin@gmail.com>
2022-12-05 13:53:54 -0600

    v0.566.38 Se integra adm tipo jrn nom

---
+++ controllers/controlador_cat_sat_tipo_jornada_nom.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\nomina\controllers;
+
+class controlador_cat_sat_tipo_jornada_nom extends \gamboamartin\cat_sat\controllers\controlador_cat_sat_tipo_jornada_nom {
+
+
+}
