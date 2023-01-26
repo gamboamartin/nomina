6396fc589deab798da5f04ddd3af68159f696180
Kevin <kevin.acuna@ivitec.com.mx>
2023-01-26 18:06:35 -0500

    v0.602.42 Se ajusta acciones de conf. percepcion

--- controllers/controlador_nom_conf_percepcion.php
+++ controllers/controlador_nom_conf_percepcion.php
@@ -8,54 +8,38 @@
  */
 namespace gamboamartin\nomina\controllers;
 
+use base\controller\controler;
 use gamboamartin\errores\errores;
+use gamboamartin\system\_ctl_base;
 use gamboamartin\system\links_menu;
-use gamboamartin\system\system;
 use gamboamartin\template\html;
-use html\nom_conf_deduccion_html;
-use html\nom_conf_nomina_html;
 use html\nom_conf_percepcion_html;
-use html\nom_deduccion_html;
-use gamboamartin\nomina\models\nom_conf_deduccion;
-use gamboamartin\nomina\models\nom_conf_nomina;
 use gamboamartin\nomina\models\nom_conf_percepcion;
 use PDO;
 use stdClass;
 
-class controlador_nom_conf_percepcion extends system {
+class controlador_nom_conf_percepcion extends _ctl_base {
 
-    public array $keys_selects = array();
-
-    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
-                                stdClass $paths_conf = new stdClass()){
+    public function __construct(PDO      $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass())
+    {
         $modelo = new nom_conf_percepcion(link: $link);
         $html_ = new nom_conf_percepcion_html(html: $html);
         $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
 
-        $columns["nom_conf_percepcion_id"]["titulo"] = "Id";
-        $columns["nom_conf_nomina_descripcion"]["titulo"] = "Conf. Nomina";
-        $columns["nom_percepcion_descripcion"]["titulo"] = "Percepcion";
-        $columns["nom_conf_percepcion_importe_gravado"]["titulo"] = "Importe Gravado";
-        $columns["nom_conf_percepcion_importe_exento"]["titulo"] = "Importe Exento";
-        $columns["nom_conf_percepcion_fecha_inicio"]["titulo"] = "Fecha Inicio";
-        $columns["nom_conf_percepcion_fecha_fin"]["titulo"] = "Fecha Fin";
+        $datatables = $this->init_datatable();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar datatable', data: $datatables);
+            print_r($error);
+            die('Error');
+        }
 
-        $filtro = array("nom_conf_percepcion.id", "nom_conf_nomina.descripcion", "nom_percepcion.descripcion",
-            "nom_conf_percepcion.importe_gravado","nom_conf_percepcion.importe_exento",
-            "nom_conf_percepcion.fecha_inicio","nom_conf_percepcion.fecha_fin");
-
-        $datatables = new stdClass();
-        $datatables->columns = $columns;
-        $datatables->filtro = $filtro;
-
-        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, datatables: $datatables,
+        parent::__construct(html: $html_, link: $link, modelo: $modelo, obj_link: $obj_link, datatables: $datatables,
             paths_conf: $paths_conf);
 
-        $this->titulo_lista = 'Configuracion Percepcion';
-
-        $propiedades = $this->inicializa_propiedades();
-        if(errores::$error){
-            $error = $this->errores->error(mensaje: 'Error al inicializar propiedades',data:  $propiedades);
+        $configuraciones = $this->init_configuraciones();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar configuraciones', data: $configuraciones);
             print_r($error);
             die('Error');
         }
@@ -63,9 +47,15 @@ class controlador_nom_conf_percepcion extends system {
 
     public function alta(bool $header, bool $ws = false): array|string
     {
-        $r_alta =  parent::alta(header: false);
-        if(errores::$error){
-            return $this->retorno_error(mensaje: 'Error al generar template',data:  $r_alta, header: $header,ws:$ws);
+        $r_alta = $this->init_alta();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar alta', data: $r_alta, header: $header, ws: $ws);
+        }
+
+        $keys_selects = $this->init_selects_inputs();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar selects', data: $keys_selects, header: $header,
+                ws: $ws);
         }
 
         $this->row_upd->fecha_inicio = date('Y-m-d');
@@ -73,107 +63,138 @@ class controlador_nom_conf_percepcion extends system {
         $this->row_upd->importe_gravado = 0;
         $this->row_upd->importe_exento = 0;
 
-        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
-        if(errores::$error){
-            $error = $this->errores->error(mensaje: 'Error al generar inputs',data:  $inputs);
-            print_r($error);
-            die('Error');
+        $inputs = $this->inputs(keys_selects: $keys_selects);
+        if (errores::$error) {
+            return $this->retorno_error(
+                mensaje: 'Error al obtener inputs', data: $inputs, header: $header, ws: $ws);
         }
 
         return $r_alta;
     }
 
-    public function asignar_propiedad(string $identificador, mixed $propiedades)
+    protected function campos_view(): array
     {
-        if (!array_key_exists($identificador,$this->keys_selects)){
-            $this->keys_selects[$identificador] = new stdClass();
+        $keys = new stdClass();
+        $keys->inputs = array('codigo', 'descripcion', 'importe_gravado', 'importe_exento');
+        $keys->fechas = array('fecha_inicio', 'fecha_fin');
+        $keys->selects = array();
+
+        $init_data = array();
+        $init_data['nom_conf_nomina'] = "gamboamartin\\nomina";
+        $init_data['nom_percepcion'] = "gamboamartin\\nomina";
+
+        $campos_view = $this->campos_view_base(init_data: $init_data, keys: $keys);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al inicializar campo view', data: $campos_view);
         }
 
-        foreach ($propiedades as $key => $value){
-            $this->keys_selects[$identificador]->$key = $value;
-        }
+        return $campos_view;
     }
 
-    private function base(): array|stdClass
+    private function init_configuraciones(): controler
     {
-        $r_modifica =  parent::modifica(header: false, ws: false);
-        if(errores::$error){
-            return $this->errores->error(mensaje: 'Error al generar template',data:  $r_modifica);
-        }
+        $this->seccion_titulo = 'Configuración de Percepciones';
+        $this->titulo_lista = 'Registros de configuraciones de Percepciones';
 
-        $this->asignar_propiedad(identificador:'nom_conf_nomina_id',
-            propiedades: ["id_selected"=>$this->row_upd->nom_conf_nomina_id]);
-        if (errores::$error) {
-            $error = $this->errores->error(mensaje: 'Error al asignar propiedad', data: $this);
-            print_r($error);
-            die('Error');
-        }
-
-        $this->asignar_propiedad(identificador:'nom_percepcion_id',
-            propiedades: ["id_selected"=>$this->row_upd->nom_percepcion_id]);
-        if (errores::$error) {
-            $error = $this->errores->error(mensaje: 'Error al asignar propiedad', data: $this);
-            print_r($error);
-            die('Error');
-        }
-
-        $inputs = $this->genera_inputs(keys_selects:  $this->keys_selects);
-        if(errores::$error){
-            return $this->errores->error(mensaje: 'Error al inicializar inputs',data:  $inputs);
-        }
-
-        $data = new stdClass();
-        $data->template = $r_modifica;
-        $data->inputs = $inputs;
-
-        return $data;
+        return $this;
     }
 
-    private function inicializa_propiedades(): array
+    private function init_datatable(): stdClass
     {
-        $identificador = "nom_conf_nomina_id";
-        $propiedades = array("label" => "Conf. Nomina");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+        $columns["nom_conf_percepcion_id"]["titulo"] = "Id";
+        $columns["nom_conf_nomina_descripcion"]["titulo"] = "Conf. Nomina";
+        $columns["nom_percepcion_descripcion"]["titulo"] = "Percepción";
+        $columns["nom_conf_percepcion_importe_gravado"]["titulo"] = "Importe Gravado";
+        $columns["nom_conf_percepcion_importe_exento"]["titulo"] = "Importe Exento";
+        $columns["nom_conf_percepcion_fecha_inicio"]["titulo"] = "Fecha Inicio";
+        $columns["nom_conf_percepcion_fecha_fin"]["titulo"] = "Fecha Fin";
 
-        $identificador = "nom_percepcion_id";
-        $propiedades = array("label" => "Percepcion");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+        $filtro = array("nom_conf_percepcion.id", "nom_conf_nomina.descripcion", "nom_percepcion.descripcion",
+            "nom_conf_percepcion.importe_gravado","nom_conf_percepcion.importe_exento", "nom_conf_percepcion.fecha_inicio",
+            "nom_conf_percepcion.fecha_fin");
 
-        $identificador = "importe_gravado";
-        $propiedades = array("place_holder" => "Importe Gravado");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+        $datatables = new stdClass();
+        $datatables->columns = $columns;
+        $datatables->filtro = $filtro;
 
-        $identificador = "importe_exento";
-        $propiedades = array("place_holder" => "Importe Exento");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+        return $datatables;
+    }
 
-        $identificador = "fecha_inicio";
-        $propiedades = array("place_holder" => "Fecha Inicio");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+    private function init_selects(array $keys_selects, string $key, string $label, int $id_selected = -1, int $cols = 6,
+                                  bool  $con_registros = true, array $filtro = array()): array
+    {
+        $keys_selects = $this->key_select(cols: $cols, con_registros: $con_registros, filtro: $filtro, key: $key,
+            keys_selects: $keys_selects, id_selected: $id_selected, label: $label);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
 
-        $identificador = "fecha_fin";
-        $propiedades = array("place_holder" => "Fecha Fin");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+        return $keys_selects;
+    }
 
-        $identificador = "codigo";
-        $propiedades = array("place_holder" => "Codigo");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+    public function init_selects_inputs(): array
+    {
+        $keys_selects = $this->init_selects(keys_selects: array(), key: "nom_conf_nomina_id", label: "Conf. Nomina");
+        return $this->init_selects(keys_selects: $keys_selects, key: "nom_percepcion_id", label: "Percepción");
+    }
 
-        $identificador = "codigo_bis";
-        $propiedades = array("place_holder" => "Codigo BIS");
-        $this->asignar_propiedad(identificador:$identificador, propiedades: $propiedades);
+    protected function key_selects_txt(array $keys_selects): array
+    {
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 12, key: 'descripcion',
+            keys_selects: $keys_selects, place_holder: 'Descripción');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
 
-        return $this->keys_selects;
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'importe_gravado',
+            keys_selects: $keys_selects, place_holder: 'Importe Gravado');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'importe_exento',
+            keys_selects: $keys_selects, place_holder: 'Importe Exento');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'fecha_inicio',
+            keys_selects: $keys_selects, place_holder: 'Fecha Inicio');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'fecha_fin',
+            keys_selects: $keys_selects, place_holder: 'Fecha Fin');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        return $keys_selects;
     }
 
     public function modifica(bool $header, bool $ws = false): array|stdClass
     {
-        $base = $this->base();
-        if(errores::$error){
-            return $this->retorno_error(mensaje: 'Error al maquetar datos',data:  $base,
-                header: $header,ws:$ws);
+        $r_modifica = $this->init_modifica();
+        if (errores::$error) {
+            return $this->retorno_error(
+                mensaje: 'Error al generar salida de template', data: $r_modifica, header: $header, ws: $ws);
         }
 
-        return $base->template;
+        $keys_selects = $this->init_selects_inputs();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar selects', data: $keys_selects, header: $header,
+                ws: $ws);
+        }
+
+        $keys_selects['nom_conf_nomina_id']->id_selected = $this->registro['nom_conf_nomina_id'];
+        $keys_selects['nom_percepcion_id']->id_selected = $this->registro['nom_percepcion_id'];
+
+        $base = $this->base_upd(keys_selects: $keys_selects, params: array(), params_ajustados: array());
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al integrar base', data: $base, header: $header, ws: $ws);
+        }
+
+        return $r_modifica;
     }
 }
--- templates/inputs/nom_conf_percepcion/alta.php
+++ templates/inputs/nom_conf_percepcion/alta.php
@@ -1,13 +1,11 @@
 <?php /** @var  \gamboamartin\nomina\controllers\controlador_nom_conf_percepcion $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
-<?php echo $controlador->inputs->codigo; ?>
-<?php echo $controlador->inputs->codigo_bis; ?>
-<?php echo $controlador->inputs->descripcion; ?>
 <?php echo $controlador->inputs->nom_conf_nomina_id; ?>
 <?php echo $controlador->inputs->nom_percepcion_id; ?>
+<?php echo $controlador->inputs->descripcion; ?>
 <?php echo $controlador->inputs->importe_gravado; ?>
 <?php echo $controlador->inputs->importe_exento; ?>
 <?php echo $controlador->inputs->fecha_inicio; ?>
 <?php echo $controlador->inputs->fecha_fin; ?>
-<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
 
