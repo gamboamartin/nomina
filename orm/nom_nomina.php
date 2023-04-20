ca2eb7d22d955067c703b7b69a9e5558bab571aa
MauricioHdz <mauricioisraelhernandez@gmail.com>
2023-04-19 23:29:11 -0600

    v1.23.12 Se ajusta funcion descarga xml

--- orm/nom_nomina.php
+++ orm/nom_nomina.php
@@ -1359,7 +1359,7 @@ class nom_nomina extends modelo
             return $this->error->error(mensaje: 'Error al obtener documento nomina', data: $r_nom_nomina_documento);
         }
 
-        $ruta_archivo = $r_nom_nomina_documento->registros[0]['doc_documento_ruta_absoluta']; /** Ruta */
+        $ruta_archivo = $r_nom_nomina_documento->registros[0]['doc_documento_ruta_absoluta'].".xml"; /** Ruta */
 
         $file_name = $nom_nomina->nom_nomina_descripcion;
 
